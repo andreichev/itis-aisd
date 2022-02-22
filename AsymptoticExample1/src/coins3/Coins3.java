12602c8055fc6ffafb9c9b9866d8c7483b01d38f
Andreichev Michail <andreichev.m@mail.ru>
2022-02-22 19:04:10 +0300

    Update Coins3.java

--- AsymptoticExample1/src/coins3/Coins3.java
+++ AsymptoticExample1/src/coins3/Coins3.java
@@ -1,8 +1,6 @@
 package coins3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Coins3 {
     static class Variant {
@@ -25,11 +23,11 @@ public class Coins3 {
         }
     }
 
-    static Variant getMinSize(List<Variant> variants) {
-        if(variants.size() == 0) { return null; }
-        Variant result = variants.get(0);
+    static Variant getMinSize(Variant[] variants) {
+        if(variants.length == 0) { return null; }
+        Variant result = variants[0];
         for(Variant variant: variants) {
-            if(variant.coins.length < result.coins.length) {
+            if(variant.coins.length < result.coins.length && variant.coins.length != 0) {
                 result = variant;
             }
         }
@@ -37,43 +35,50 @@ public class Coins3 {
     }
 
     public static void main(String[] args) {
-        int sum = 987;
+        int sum = 42;
         Variant[] variants = new Variant[sum + 1];
         variants[0] = new Variant(new int[] {});
         variants[1] = new Variant(new int[] {1});
-        List<Variant> tempVariants = new ArrayList<>();
+        Variant[] tempVariants = new Variant[6];
         for(int i = 2; i <= sum; i++) {
-            tempVariants.clear();
             if(i == 5) {
-                tempVariants.add(new Variant(new int[]{5}));
+                variants[i] = new Variant(new int[]{5});
+                continue;
             }
             if(i == 10) {
-                tempVariants.add(new Variant(new int[]{10}));
+                variants[i] = new Variant(new int[]{10});
+                continue;
             }
             if(i == 20) {
-                tempVariants.add(new Variant(new int[]{20}));
+                variants[i] = new Variant(new int[]{20});
+                continue;
             }
             if(i == 25) {
-                tempVariants.add(new Variant(new int[]{25}));
+                variants[i] = new Variant(new int[]{25});
+                continue;
             }
             if(i == 50) {
-                tempVariants.add(new Variant(new int[]{50}));
+                variants[i] = new Variant(new int[]{50});
+                continue;
             }
-            tempVariants.add(variants[i - 1].getVariantAddingCoin(1));
+            for (int j = 0; j < 6; j++) {
+                tempVariants[j] = new Variant(new int[] {});
+            }
+            tempVariants[0] = variants[i - 1].getVariantAddingCoin(1);
             if(i > 5) {
-                tempVariants.add(variants[i - 5].getVariantAddingCoin(5));
+                tempVariants[1] = variants[i - 5].getVariantAddingCoin(5);
             }
             if(i > 10) {
-                tempVariants.add(variants[i - 10].getVariantAddingCoin(10));
+                tempVariants[2] = variants[i - 10].getVariantAddingCoin(10);
             }
             if(i > 20) {
-                tempVariants.add(variants[i - 20].getVariantAddingCoin(20));
+                tempVariants[3] = variants[i - 20].getVariantAddingCoin(20);
             }
             if(i > 25) {
-                tempVariants.add(variants[i - 25].getVariantAddingCoin(25));
+                tempVariants[4] = variants[i - 25].getVariantAddingCoin(25);
             }
             if(i > 50) {
-                tempVariants.add(variants[i - 50].getVariantAddingCoin(50));
+                tempVariants[5] = variants[i - 50].getVariantAddingCoin(50);
             }
             variants[i] = getMinSize(tempVariants);
         }
